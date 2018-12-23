eb7fc30483236a564224d55ff54e82076c99dd44
Abdulmajeed Alomari <abdulmajeedmmo@gmail.com>
2018-12-24 02:55:47 +0300

    chore: Add User model relations to database

--- database_setup.py
+++ database_setup.py
@@ -13,6 +13,7 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
+    picture = Column(String(250))
 
 
 class Category(Base):
@@ -38,8 +39,8 @@ class Item(Base):
     description = Column(String(250))
     category_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(Category)
-    # user_id = Column(Integer, ForeignKey('user.id'))
-    # user = relationship(User)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
     created_date = Column(DateTime, default=datetime.datetime.utcnow)
 
     @property
